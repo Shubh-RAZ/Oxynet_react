5dd201b0f9c99b14eb49da749a6540d11b7648c5
amar567 <60555078+amar567@users.noreply.github.com>
2021-04-25 16:34:35 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Homepage from './Components/Homepage';
-import About from './Components/about_us/about'
+// import About from './Components/about_us/about'
 
 function App() {
   return (
     <div className="App">
-     {/* <Homepage></Homepage> */}
-     <About></About>
+     <Homepage/>
+     {/* <About></About> */}
     </div>
   );
 }
