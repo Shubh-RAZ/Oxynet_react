599fbd56227cf263fd79ba24b841c8cebd06ffb4
amar567 <60555078+amar567@users.noreply.github.com>
2021-04-30 15:25:34 +0530

    Update SupplierDashboard.js

--- src/Components/supplier-dashboard/SupplierDashboard.js
+++ src/Components/supplier-dashboard/SupplierDashboard.js
@@ -65,6 +65,7 @@ constructor(props){
                     reportedBy:'10'
                 }
             ],
+            editcard:[],
             district:'Reset district',
             stateName: '',
             districts: [],
@@ -168,31 +169,31 @@ constructor(props){
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.quantity} type="text" className="in-input" placeholder="Quantity (eg. 100 Oxygen Cylinders)" required></input>
+                                        <input defaultValue={this.state.editcard.quantity} type="text" className="in-input" placeholder="Quantity (eg. 100 Oxygen Cylinders)" required></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.cost} type="text" className="in-input" placeholder="Cost (MRP) per cylinder" required></input>
+                                        <input defaultValue={this.state.editcard.cost} type="text" className="in-input" placeholder="Cost (MRP) per cylinder" required></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.phone1} type="text" className="in-input" placeholder="10 digit Phone Number" required></input>
+                                        <input defaultValue={this.state.editcard.phone1} type="number" className="in-input" placeholder="10 digit Phone Number" required></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.phone2} type="text" className="in-input" placeholder="Alternate Phone Number" ></input>
+                                        <input defaultValue={this.state.editcard.phone2} type="number" className="in-input" placeholder="Alternate Phone Number" ></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.shopname} type="text" className="in-input" placeholder="Shop Name" required></input>
+                                        <input defaultValue={this.state.editcard.shopname} type="text" className="in-input" placeholder="Shop Name" required></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field">
-                                        <input defaultValue={this.props.shopadd} type="text" className="in-input" placeholder="Shop Address" required></input>
+                                        <input defaultValue={this.state.editcard.shopadd} type="text" className="in-input" placeholder="Shop Address" required></input>
                                     </div>
                                     <div style={{height:'30px'}}></div>
                                     <div className="input-field" >
-                                        <select className="in-input" name="stateName" id="state" defaultValue={this.props.state} onChange={this.myChangeHandler} required>
+                                        <select className="in-input" name="stateName" id="state" defaultValue={this.state.editcard.state} onChange={this.myChangeHandler} required>
                                             <option value="" disabled >Select state</option>
                                             {states.map((state,index) => (<option key={index} name={state}>{state}</option>))}
                                         </select>
@@ -291,153 +292,3 @@ constructor(props){
         )
     }
 }
-
-
-// import React, { Component } from 'react'
-// import './SupplierDashboard.css'
-// import SupplyTop from '../SupplyTop'
-// import Editcard from '../Editcard';
-
-// export default class SupplierDashboard extends Component {
-    
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//         showform:false,
-//         render:[
-//             {
-//                 Id : 'kuch toh bhi',
-//                 city: 'Akola',
-//                 quantity:'80',
-//                 shopName:'Shivh`s oxygen',
-//                 address:'Kala chabutra, patli gali, timbaktu ',
-//                 cost:'12000',
-//                 phoneNo1:'8695432138',
-//                 phoneNo2:'6381365485',
-//                 lastUpdate:'10:25',
-//                 reportedBy:'10'
-//             }
-//             ,
-//             {
-//                 Id : 'kuch toh bhi',
-//                 city: 'Akola',
-//                 quantity:'80',
-//                 shopName:'Shivh`s oxygen',
-//                 address:'Kala chabutra, patli gali, timbaktu ',
-//                 cost:'12000',
-//                 phoneNo1:'8695432138',
-//                 phoneNo2:'6381365485',
-//                 lastUpdate:'10:25',
-//                 reportedBy:'10'
-//             }
-//             ,
-//             {
-//                 Id : 'kuch toh bhi',
-//                 city: 'Akola',
-//                 quantity:'80',
-//                 shopName:'Shivh`s oxygen',
-//                 address:'Kala chabutra, patli gali, timbaktu ',
-//                 cost:'12000',
-//                 phoneNo1:'8695432138',
-//                 phoneNo2:'6381365485',
-//                 lastUpdate:'10:25',
-//                 reportedBy:'10'
-//             }
-//         ]
-//     }
-// }
-
-// handleform = () => {
-//     this.setState({
-//         showform:true
-//     })
-// }
-
-// handledelete = () => {
-    
-// }
-
-// handlecancel = () => {
-//     this.setState({
-//         showform:false
-//     })
-// }
-
-//     render() {
-//         console.log('Show form '+this.state.showform)
-//         return (
-//             <div className="dashboard-whole"  style={{background: this.state.showform ? 'grey' : 'white'}} >
-                
-//                 <SupplyTop></SupplyTop>
-                
-//                 <div className="supply_start">
-//                     {this.state.showform ? <div className="cancel-svg" onClick={this.handlecancel}>
-//                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-//                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-//                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-//                         </svg></div> : null 
-//                     }
-
-                    
-//                     {this.state.showform ? <SupplierForm></SupplierForm> : 
-//                         <div className="row row-class">
-//                             {this.state.render.map((obj,index)=>{return (
-//                                                                             <div className="col-md-3 cards" style={{width: '100%',padding:'15px 0'}} >
-//                                                                                 <div className="card-inside">
-//                                                                                     <div className="card-name">{obj.shopName}</div>
-//                                                                                     <div className="card-add">{obj.address}</div>
-//                                                                                     <div className="av">
-//                                                                                         <div className="card-avail">
-//                                                                                             <div className="avail">Availaibility</div>
-//                                                                                             <div className="avail"><h1>{obj.quantity}</h1></div>
-//                                                                                             <div className="avail">Last updated at: {obj.lastUpdate}</div>
-//                                                                                             <div style={{'color':'white','fontSize':'15px'}}>Oxygen Cylinders</div>
-//                                                                                         </div>
-//                                                                                     </div>
-//                                                                                     <div className="rate">Rs . {obj.cost} / Cylinder</div>
-//                                                                                     <div className="phone">
-//                                                                                         <div className="phone-svg">
-//                                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white" className="bi bi-telephone" viewBox="0 0 16 16">
-//                                                                                         <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-//                                                                                         </svg></div>
-//                                                                                         <div className="phone-detail">
-//                                                                                             <a href={`tel:`+obj.phoneNo1}>
-//                                                                                                 <font style={{'color':'blue'}}>
-//                                                                                                     +91-{obj.phoneNo1}
-//                                                                                                 </font>
-//                                                                                             </a> , 
-//                                                                                             <a href={`tel:`+obj.phoneNo2}>
-//                                                                                                 <font style={{'color':'blue'}}>
-//                                                                                                     +91-{obj.phoneNo2}
-//                                                                                                 </font>
-//                                                                                             </a>
-//                                                                                         </div>    
-//                                                                                     </div>
-//                                                                                     <div className="service">
-//                                                                                         <button type="button" className="report-btn" onClick={this.handledelete} style={{width:'80px'}}>Delete</button>
-//                                                                                         <div style={{width:'50px'}}></div>
-//                                                                                         <button type="button" className="report-btn" onClick={this.handleform} style={{width:'80px',background:'green'}}>Edit</button>
-//                                                                                     </div>
-//                                                                                     <font>
-//                                                                                         Reported by {obj.reportedBy}
-//                                                                                     </font>
-//                                                                                 </div>
-//                                                                             </div>
-//                             )})}
-//                             <div className="col-md-3 btn-box">
-//                                 <div className="inside-btn">
-//                                     <div className="add-btn" onClick={this.handleform}>
-//                                         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="white" className="bi bi-plus" viewBox="0 0 16 16">
-//                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-//                                         </svg>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     }
-//                 </div>
-                
-//             </div>
-//         )
-//     }
-// }
