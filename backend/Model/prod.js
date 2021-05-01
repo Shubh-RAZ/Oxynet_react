d759daf38315c95e8053cf49f03e3d0ec6229b5c
Talha-shamim <66217619+Talha-shamim@users.noreply.github.com>
2021-05-01 12:17:16 +0530

    Update prod.js

--- backend/Model/prod.js
+++ backend/Model/prod.js
@@ -1,50 +1,5 @@
 import mongoose from 'mongoose';
 
-const CardSchema = new mongoose.Schema({
-    outofstock:{
-        type:Boolean,
-        default:false
-    },
-    quantity :  {
-        type : String,
-        required : true
-    },
-    shopName :  {
-        type : String,
-        required : true
-    },
-    address :  {
-        type : String,
-        required : true
-    },
-    city :  {
-        type : String,
-        required : true
-    },
-    state :  {
-        type : String,
-        required : true
-    },
-    cost : {
-        type : Number,
-        required : true
-    },
-    phoneNo1 : {
-        type : Number,
-        required : true
-    },
-    phoneNo2 :  {
-        type : Number,
-        required : true
-    },
-    reported : [
-        {
-            email : String,
-            reason : String,
-        }
-    ]
-})
-
 const ProdSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -57,17 +12,53 @@ const ProdSchema = new mongoose.Schema({
         type:String,
         unique: true
     },
-	uname:{
-        type:String,
-        unique: false
-    },
-    card:[CardSchema]
+    card:[
+        {
+            outofstock:{
+                type:Boolean,
+                default:false
+            },
+            quantity :  {
+                type : String,
+                required : true
+            },
+            shopName :  {
+                type : String,
+                required : true
+            },
+            address :  {
+                type : String,
+                required : true
+            },
+            city :  {
+                type : String,
+                required : true
+            },
+            state :  {
+                type : String,
+                required : true
+            },
+            cost : {
+                type : Number,
+                required : true
+            },
+            phoneNo1 : {
+                type : Number,
+                required : true
+            },
+            phoneNo2 :  {
+                type : Number,
+                required : true
+            },
+            reported : [
+                {
+                    email : String,
+                    reason : String,
+                }
+            ]
+        }
+    ]
 });
 
 const Prod = mongoose.model('Prod', ProdSchema);
 export default  Prod;
-
-// const Card = mongoose.model('Card', CardSchema)
-// export{Card}
-
-// so when we will make query for certain city we should get alll the cards having that city as parameter
