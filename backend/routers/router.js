831807ad97fcb63b118c24ed29d6dca8293deffa
Talha-shamim <talhashamim001@gmail.com>
2021-05-01 12:06:55 +0530

    backend

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { getAllProd, addProduct, getByCity, deleteProd, report, getById, UpdateById } from '../Controllers/controller.js';
+import { getAllProd, addProduct, getByCity, report, getById, UpdateById, addProducer, deleteAllByEmail, deleteCardByEmail, reportArray } from '../Controllers/controller.js';
 
 const router = express.Router();
 router.get('/', getAllProd);
 router.post('/addProduct', addProduct);
+router.post('/addProducer', addProducer);
 router.get('/getByCity?', getByCity);
-router.delete('/delete', deleteProd);
+router.delete('/deleteAll', deleteAllByEmail);
+router.delete('/deleteCard', deleteCardByEmail);
 router.post('/report', report);
+router.post('/reportArray', reportArray);
 router.get('/getById/?', getById);
-router.put('/upadteById/?' , UpdateById);
+router.post('/updateById' , UpdateById);
 
 export default router;
