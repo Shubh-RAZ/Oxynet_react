5548c0783a711554c8b5620b23a5dce08b9c5d6f
Shubh-RAZ <ce19b030@iittp.ac.in>
2021-05-06 18:23:30 +0530

    new-commit

--- src/Components/supplier-dashboard/SupplierDashboard.js
+++ src/Components/supplier-dashboard/SupplierDashboard.js
@@ -321,7 +321,7 @@ constructor(props){
                                 <div className="rate">Rs . {obj.cost} / Cylinder</div>
                                 <div className="phone">
                                     <div className="phone-svg">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="white" className="bi bi-telephone" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="bi bi-telephone" viewBox="0 0 16 16">
                                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                                     </svg></div>
                                     <div className="phone-detail">
@@ -330,6 +330,7 @@ constructor(props){
                                                 +91-{obj.phoneNo1}
                                             </font>
                                         </a> , 
+                                        <br></br>
                                         <a href={`tel:`+obj.phoneNo2}>
                                             <font style={{'color':'blue'}}>
                                                 +91-{obj.phoneNo2}
@@ -338,9 +339,19 @@ constructor(props){
                                     </div>    
                                 </div>
                                 <div className="service">
-                                    <button type="button" className="report-btn" onClick={()=>{this.handledelete(obj.Id,index)}} style={{width:'80px'}}>Delete</button>
+                                    <button type="button" className="del-btn" onClick={()=>{this.handledelete(obj.Id,index)}} style={{width:'80px'}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#6F50FF" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>
+Delete
+                                    </button>
                                     <div style={{width:'50px'}}></div>
-                                    <button type="button" className="report-btn" onClick={()=>{this.handleEdit(index)}} style={{width:'80px',background:'green'}}>Edit</button>
+                                    <button type="button" className="editt-btn" onClick={()=>{this.handleEdit(index)}} style={{width:'80px'}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#6F50FF" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+</svg>
+Edit
+                                    </button>
                                 </div>
                                 <div style={{height:'5px'}}></div>
                                 <font>
